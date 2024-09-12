bf101501d128209fdbb9504136ba74609c605f7a
busarinajim <busarinajim@gmail.com>
2024-09-12 00:12:21 +0000

    prints 00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i <= 98:
-        print(f"{i // 10}{i % 10}", end=", ")
+    if i == 99:
+        print("{}".format(i))
     else:
-        print(f"{99}")
+        print(f"{i // 10}{i % 10}", end=", ")
 
