1a71a2bf4783a56180b97ca6aa7571f82efb06a6
Najim <busarinajim>
2024-12-16 20:25:34 -0800

    Prints multi line sentences using loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const sentence = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array and print each sentence
+for (let i = 0; i < sentence.length; i++) {
+  console.log(sentence[i]);
+}
